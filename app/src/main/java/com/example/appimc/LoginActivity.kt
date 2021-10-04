cc7999aec0f1a8934991addfb6984f8091a2a638
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-10-04 11:26:43 -0300

    Tela novo usuario

--- app/src/main/java/com/example/appimc/LoginActivity.kt
+++ app/src/main/java/com/example/appimc/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.appimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val textViewNovaConta = findViewById<TextView>(R.id.text_view_nova_conta)
+
+        textViewNovaConta.setOnClickListener {
+            val novaConta = Intent(this, NovoUsuarioActivity)
+            startActivity(novaConta)
+        }
     }
 }
--- app/src/main/java/com/example/appimc/NovoUsuarioActivity.kt
+++ app/src/main/java/com/example/appimc/NovoUsuarioActivity.kt
@@ -2,10 +2,18 @@ package com.example.appimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
-class PerfilActivity : AppCompatActivity() {
+class NovoUsuarioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        supportActionBar!!.title = "PERFIL"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_perfil, menu)
+        return true
     }
 }
